0eeab120cb3c09710eaaba6b11ced4a30b8570f1
Daniel Aberger <da@ixab.de>
2022-03-08 22:23:16 +0100

    add helper function and basic output

--- leetoclock.go
+++ leetoclock.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/sirupsen/logrus"
 )
@@ -14,6 +17,23 @@ func Start(discord *discordgo.Session) {
 	discord.AddHandler(onMessageCreate)
 }
 
+func idToTimestamp(id string) (int64, error) {
+	convertedID, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		return -1, err
+	}
+	convertedIDString := strconv.FormatInt(convertedID, 2)
+	m := 64 - len(convertedIDString)
+	unixbin := convertedIDString[0 : 42-m]
+	unix, err := strconv.ParseInt(unixbin, 2, 64)
+	if err != nil {
+		return -1, err
+	}
+	return unix + 1420070400000, nil
+}
+
 func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	logrus.Infoln("leetoclock onMessageCreate handler called")
+	timestamp, _ := idToTimestamp(m.ID)
+	tm := time.Unix(timestamp/1000, 0)
+	logrus.Infof("leetoclock: message has unix timestamp %s which equals %s\n", timestamp, tm)
 }
